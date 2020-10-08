3e22afbeb7596c355336606b221699af087b9bc3
wentao cao <wcao18@asu.edu>
2020-10-07 22:41:57 -0700

    second change

--- cse360assignment02/src/cse360assignment02/AddingMachine.java
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -1,26 +1,41 @@
 package cse360assignment02;
 
 public class AddingMachine {
-  private int total;
+	private int total;
+	private String history;
+	
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+		history = "0";
+	}
+	
+	public int getTotal () {
+		return total;
+	}
   
-  public AddingMachine () {
-    total = 0;  // not needed - included for clarity
-  }
-  
-  public int getTotal () {
-    return 0;
-  }
-  
-  public void add (int value) {
-  }
+	public void add (int value) {
+		total = total + value;
+		history = history + " + " + value;
+	}
 
-  public void subtract (int value) {
-  }
+	public void subtract (int value) {
+		total = total - value;
+		history = history + " - " + value;
+	}
 
-  public String toString () {
-    return "";
-  }
+	public String toString () {
+		return history;
+	}
 
-  public void clear() {
-  }
+	public void clear() {
+		history = "0";
+	}
+	
+	public static void main(String[] args) {
+		AddingMachine cal = new AddingMachine();
+		cal.add(4);
+		cal.subtract(2);
+		cal.add(5);
+		System.out.print(cal.toString());
+	}
 }
